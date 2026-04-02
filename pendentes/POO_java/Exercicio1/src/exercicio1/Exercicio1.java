aff08c6a0d3ee05da8a4c610d65e102324fb7982
unknown <joaocavassini@gmail.com>
2026-04-02 19:00:02 -0300

    Upload diário: Exercicio1

---
+++ pendentes/POO_java/Exercicio1/src/exercicio1/Exercicio1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio1;
+
+/**
+ *
+ * @author Seceip
+ */
+public class Exercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int var1 = 5;
+        int var2 = 2;
+        
+        System.out.println("var 1 = " + var1);
+        System.out.println("var 2 = " + var2);
+        System.out.println("var 1 + var 2 = " + (var1+var2));
+        System.out.println("var 1 - var 2 = " + (var1-var2));
+        System.out.println("var 1 * var 2 = " + (var1*var2));
+        System.out.println("var 1 / var 2 = " + (var1/var2));
+        System.out.println("resto de var 1 / var 2 = " + (var1%var2));
+        
+        
+    }
+    
+}
